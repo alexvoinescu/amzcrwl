18d3da0f66d0a992619efd9468f30247e54b1ebf
Alexandru Voinescu <alexandru.voinescu@gameloft.com>
2017-12-18 19:54:58 +0200

    +first version

---
+++ src/main/java/org/pyro/amzcrawl/AmzCrwlApplication.java
@@ -0,0 +1,12 @@
+package org.pyro.amzcrawl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmzCrwlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmzCrwlApplication.class, args);
+	}
+}
